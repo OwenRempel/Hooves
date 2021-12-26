212c2e752a7461040feabd2b9cd386aeca137ccd
Owen Rempel <owenrempel@gmail.com>
2021-12-25 17:01:56 -0700

    Starting the api view system

--- API/app/index.php
+++ API/app/index.php
@@ -1,10 +1,16 @@
 <?php 
 require_once("Database/DB.php");
+header('Content-Type: application/json; charset=utf-8');
 
 if(!is_file('Build/Built')){
     DB_Admin::mkDB('HovesAdmin');
     DB_Admin::exFile(file_get_contents('Database/sql/admin.sql'));
     touch('Build/Built');
 }
+$url = explode('/', $_SERVER['REQUEST_URI']);
+$method = $_SERVER['REQUEST_METHOD'];
 
-print_r($_GET['url']);
+$viewCheck = './Views/'.$url[0].'.php';
+if(is_file($viewCheck)){
+    include($viewCheck);
+}
