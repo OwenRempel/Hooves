fd1960cf2ab527b059eeccc3e5f94d9a0f1cd1bd
Owen Rempel <owenrempel@gmail.com>
2021-12-30 22:07:10 -0700

    Updated FormBuilderArray to have more fields

--- API/app/Extras/FormBuilderArray.php
+++ API/app/Extras/FormBuilderArray.php
@@ -1,57 +1,157 @@
 <?php
 
+/*
+
+//These are some templates for how the forms will work
+
+[
+    'name'=>'UserEmail',
+    'typeName'=>'FormSelect',
+    'selectLabel'=>'User',
+    'options'=>[
+        [
+            "value"=>'Owen',
+            "option"=>'owen'
+        ],
+        [
+            "value"=>'Terrilee',
+            "option"=>'terrilee'
+        ],
+        [
+            "value"=>'Dustin',
+            "option"=>'dustin'
+        ]  
+    ]
+    
+],
+
+[
+    'name'=>'Age',
+    'typeName'=>'FormCheckbox',
+    'checkboxTitle'=>'18+',
+    'checkboxLabel'=>'Age',
+    "required"=>true
+],
+
+
+[
+    'name'=>'Message',
+    'typeName'=>'FormTextarea',
+    'placeolder'=>'Type your message here',
+    'textareaLabel'=>'Message'
+    
+]
+
+
+
+*/
+
+
+
 $FormBuilderArray = [
-    'Forms'=>[
-        'company'=>[
-            'add'=>[
-                'formTitle'=>'Add Company',
-                'formName'=>'CompanyAddItem',
-                'items'=>[
-                    [
-                        'name'=>'CompanyName',
-                        'typeName'=>'FormInput',
-                        'type'=>'text',
-                        'inputLabel'=>'Company Name',
-                        
-                    ],
-                    [
-                        'name'=>'UserEmail',
-                        'typeName'=>'FormInput',
-                        'type'=>'text',
-                        'inputLabel'=>'Email'
-                        
-                    ],
-                    [
-                        'name'=>'UserPassword',
-                        'typeName'=>'FormInput',
-                        'type'=>'password',
-                        'inputLabel'=>'Password'
-                        
-                    ],
-                    [
-                        'name'=>'age',
-                        'typeName'=>'FormInput',
-                        'type'=>'text',
-                        'inputLabel'=>'Enter your age',
-                        
-                        
-                    ],
-                    [
-                        'name'=>'address',
-                        'typeName'=>'FormInput',
-                        'type'=>'text',
-                        'inputLabel'=>'Address'
-                        
-                    ],
-                    [
-                        'name'=>'hometown',
-                        'typeName'=>'FormInput',
-                        'type'=>'text',
-                        'inputLabel'=>'Home town'
-                        
+    'Routes'=>[
+        'companies'=>[
+            'formTitle'=>'Add Company',
+            'formName'=>'CompanyAddItem',
+            'passwordCheck'=>'UserPassword',
+            'tableName'=>'Companies',
+            'secondTableName'=>'Users',
+            'items'=>[
+                [
+                    'name'=>'CompanyName',
+                    'typeName'=>'FormInput',
+                    'type'=>'text',
+                    'inputLabel'=>'Company Name',
+                    
+                ],
+                [
+                    'name'=>'UserEmail',
+                    'typeName'=>'FormInput',
+                    'type'=>'Email',
+                    'inputLabel'=>'User Email',
+                    'secondTable'=>True
+                    
+                ],
+                [
+                    'name'=>'UserPassword',
+                    'typeName'=>'FormInput',
+                    'type'=>'password',
+                    'inputLabel'=>'Password',
+                    'secondTable'=>True                    
+                ],
+                [   //TODO: Work on getting the passwords match to work before the form is submited
+                    'name'=>'UserPassword-confirm',
+                    'typeName'=>'FormInput',
+                    'type'=>'password',
+                    'inputLabel'=>'Confirm Password'
+                ]
+            ]
+        ],
+        'server'=>[
+            'view'=>True
+        ],
+        'users'=>[
+            'formTitle'=>'Add Company',
+            'formName'=>'CompanyAddItem',
+            'items'=>[
+                [
+                    'name'=>'CompanyName',
+                    'typeName'=>'FormInput',
+                    'type'=>'text',
+                    'inputLabel'=>'Company Name',
+                    
+                ],
+                [
+                    'name'=>'UserEmail',
+                    'typeName'=>'FormSelect',
+                    'selectLabel'=>'User',
+                    'options'=>[
+                        [
+                            "value"=>'Owen',
+                            "option"=>'owen'
+                        ],
+                        [
+                            "value"=>'Terrilee',
+                            "option"=>'terrilee'
+                        ],
+                        [
+                            "value"=>'Dustin',
+                            "option"=>'dustin'
+                        ]  
                     ]
+                    
+                ],
+                [
+                    'name'=>'UserPassword',
+                    'typeName'=>'FormInput',
+                    'type'=>'password',
+                    'inputLabel'=>'Password'
+                    
+                ],
+                [
+                    'name'=>'Age',
+                    'typeName'=>'FormCheckbox',
+                    'checkboxTitle'=>'18+',
+                    'checkboxLabel'=>'Age',
+                    "required"=>true
+                    
+                ],
+                [
+                    'name'=>'Address',
+                    'typeName'=>'FormInput',
+                    'type'=>'text',
+                    'inputLabel'=>'Address'
+                    
+                ],
+                [
+                    'name'=>'Message',
+                    'typeName'=>'FormTextarea',
+                    'placeolder'=>'Type your message here',
+                    'textareaLabel'=>'Message'
+                    
                 ]
             ]
         ]
+
     ]
 ];
