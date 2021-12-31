e826e1c4f67ab8338a038e7d4732f3eb10fdf926
Owen Rempel <owenrempel@gmail.com>
2021-12-30 22:09:17 -0700

    Miss... Cleanup

--- API/app/Views/company.php
+++ API/app/Views/company.php
@@ -1,11 +1,22 @@
 <?php
 
-
 if(isset($Routes[1])){
     if($Routes[1] == 'add'){
         $LocalArray = $FormBuilderArray['Forms']['company']['add'];
         $SendArray = [];
-        $FormCopyArray = ['name', 'type', 'typeName', 'inputLabel', 'placeholder'];
+        $FormCopyArray = [
+            'name',
+            'type',
+            'typeName',
+            'checkboxLabel',
+            'required',
+            'selectLabel',
+            'textareaLabel', 
+            'options',
+            'inputLabel',
+            'placeholder',
+            'checkboxTitle'
+        ];
         foreach($LocalArray['items'] as $RebuildItem){  
             $TempLocalArray = [];  
             foreach($FormCopyArray as $CopyName){
@@ -23,20 +34,21 @@ if(isset($Routes[1])){
             echo json_encode($SendArray);
         }elseif($method == 'POST'){
             $SendFormBuilder = [];
-            $entityBody = file_get_contents('php://input');
-            echo $entityBody;
-            if(isset($_POST[$LocalArray['formName']])){
-                foreach($_POST as $itemKey => $item){
+            $PostData = json_decode(file_get_contents('php://input'), 1);
+            if(isset($PostData[$LocalArray['formName']])){
+                foreach($PostData as $itemKey => $item){
                     foreach($LocalArray['items'] as $formItem){
                         if($itemKey == $formItem['name']){
-                            $SendFormBuilder[][$formItem['name']] = $item;
+                            $SendFormBuilder[$formItem['name']] = $item;
                             continue;
                         }
                     }
                 }
-                //echo json_encode($SendFormBuilder);
+                echo json_encode(['Sucess'=>1, "Message"=>'Company Sucessfuly Added']);
+            }else{
+                echo json_encode(['Error'=>1, "Message"=>'Incorect URL']);
             }
-            //echo json_encode(['There was an error']);
+            
         }
     }
 }
