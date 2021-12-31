2ca41698974463df891418556d15b2c296c430ff
Owen Rempel <owenrempel@gmail.com>
2021-12-30 22:06:36 -0700

    more form tweaks added a redirect for when form is submited

--- UI/hooves_ui/pages/company/add.js
+++ UI/hooves_ui/pages/company/add.js
@@ -1,32 +1,38 @@
 import Form from "../../Components/Form"
+import Router from 'next/router'
 
 function  add({ formItems }) {
     const { callBack } = formItems;
     const returnFormData = async e => {
         e.preventDefault();
         const FormItem = e.target;
-        let FormOut = new FormData();
-        //let FormOut = {};
+        //let FormOut = new FormData();
+        let FormOut = {};
         for (let i = 0; i < FormItem.length; i++) {
             const item = FormItem[i];   
-            FormOut.append(item.name, item.value);         
-            //FormOut[item.name] = item.value;
+            //FormOut.append(item.name, item.value);         
+            FormOut[item.name] = item.value;
         }
         console.log(FormOut)
-        const res = await fetch('http://localhost/company/add', {
+        const res = await fetch(callBack, {
             method: 'POST',
-            body: FormOut
+            headers:{
+              'Content-Type': '"application/x-form-urlencoded"'
+            },
+            body: JSON.stringify(FormOut)
           });
         
-        const formdata = await res.text();
-        console.log(formdata);
+        const Response = await res.json();
+        console.log(Response);
+        e.target.reset();
+        Router.push('/');
     }
     return <Form onSubmit={returnFormData}  {...formItems} />
 }
 
 
 export async function getStaticProps() {
-    const res = await fetch('http://localhost/company/add')
+    const res = await fetch('http://localhost/companies/add')
     const formItems = await res.json()
     return {
       props: {
