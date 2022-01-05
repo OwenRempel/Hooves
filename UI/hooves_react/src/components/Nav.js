8809244c9a78181881885a249d06293617828b81
Owen Rempel <owenrempel@gmail.com>
2022-01-05 11:25:35 -0700

    Added Matrial Icons to the app and converted the nav over to them

--- UI/hooves_react/src/components/Nav.js
+++ UI/hooves_react/src/components/Nav.js
@@ -3,10 +3,30 @@ import { Link } from "react-router-dom"
 function Nav() {
     return (
         <nav className='menu'>
-            <Link to='/' ><div><img src='/images/menu/Cat.png' alt="" className='menuIcon' /><span>Home</span></div></Link>
-            <Link to='/list' ><div><img src='/images/menu/Cat.png' alt="" className='menuIcon' /><span>List    </span></div></Link>
-            <Link to='/companies' ><div><img src='/images/menu/Cat.png' alt="" className='menuIcon' /><span>Companies   </span></div></Link>
-            <Link to='/auth' ><div><img src='/images/menu/Cat.png' alt="" className='menuIcon' /><span>Auth    </span></div></Link>
+            <Link to='/' >
+                <div>
+                    <span className="material-icons menuIcon">home</span>
+                    <span>Home</span>
+                </div>
+            </Link>
+            <Link to='/cows' >
+                <div>
+                    <span className="material-icons menuIcon">format_list_bulleted</span>
+                    <span>List</span>
+                </div>
+            </Link>
+            <Link to='/settings'>
+                <div>
+                    <span className="material-icons menuIcon">settings</span>
+                    <span>Settings</span>
+                </div>
+            </Link>
+            <Link to='/logout' >
+                <div>
+                    <span className="material-icons menuIcon">logout</span>
+                    <span>Logout</span>
+                </div>
+            </Link>
         </nav>
     )
 }
