59dab5b1d0f83d6c3d45f0dfe9930747a828c38c
Owen Rempel <owenrempel@gmail.com>
2022-01-06 11:31:21 -0700

    Added in the form system for adding a cow moved the form submit logic to another function

--- UI/hooves_react/src/components/routes/Cows/CowsAdd.js
+++ UI/hooves_react/src/components/routes/Cows/CowsAdd.js
@@ -1,10 +1,29 @@
-
-
+import React, {useEffect, useState} from 'react';
+import Form from "../../Form";
+import { formHandle } from '../../../lib/FormHandle';
+import { Link } from 'react-router-dom';
 function CowsAdd() {
+    const [formData, setFormData] = useState({});
+    useEffect(() => {
+      fetch('http://localhost/cattle/info')
+            .then(response => response.json())
+            .then(result => {
+              setFormData(result)
+            });
+    }, [])
+    
+    
+    
+    const returnFormData = async e => {
+        e.preventDefault();
+        const res = formHandle(formData, e.target);
+        console.log(res)
+    }
     return (
-        <div>
-            
-        </div>
+      <div className="container">
+        <Link to='/'><button className='btn'>Home</button></Link>
+        {formData.form && <Form onSubmit={returnFormData}  {...formData} />}
+      </div>
     )
 }
 
---
+++ UI/hooves_react/src/lib/FormHandle.js
@@ -0,0 +1,34 @@
+export async function formHandle(formData, FormItem, searchParams){
+    const { form } = formData
+    const { callBack, passwordCheck } = form;
+    if(passwordCheck){
+    const pass1 = FormItem.querySelector('#'+passwordCheck[0]);
+    const pass2 = FormItem.querySelector('#'+passwordCheck[1]);
+        if(pass1.value !== pass2.value){
+            pass1.value = '';
+            pass2.value = '';
+            alert("Passwords Dont match");
+            return
+        }
+    }
+    let FormOut = {};
+    for (let i = 0; i < FormItem.length; i++) {
+        const item = FormItem[i];   
+        //FormOut.append(item.name, item.value);         
+        FormOut[item.name] = item.value;
+    }
+    const token = localStorage.getItem('Token');
+    FormOut['Token'] = token;
+    console.log(FormOut)
+    
+    const res = await fetch(callBack+'/add', {
+        method: 'POST',
+        headers:{
+        'Content-Type': '"application/x-form-urlencoded"'
+        },
+        body: JSON.stringify(FormOut)
+    });
+    
+    const Response = await res.json();
+    return Response
+}
