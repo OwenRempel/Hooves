733774646385e8b8ab5b768aad09d322fe8c15b9
Owen Rempel <owenrempel@gmail.com>
2022-01-06 16:27:17 -0700

    Builder for the display array

--- API/app/Views/server.php
+++ API/app/Views/server.php
@@ -1,4 +1,16 @@
 <?php 
-
+include('../Extras/FormBuilderArray.php');
 
 echo json_encode($_SERVER);
+
+/*
+global $FormBuilderArray;
+ 
+$out_array = [];
+foreach($FormBuilderArray['Routes']['cattle']['items'] as $item){
+    $out_array[$item['name']] = True;
+}
+
+echo json_encode($out_array);
+
+*/
