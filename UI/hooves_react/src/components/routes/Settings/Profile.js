c363e9117a773e104c73263fd64280023078c400
Owen Rempel <owenrempel@gmail.com>
2022-01-06 17:04:26 -0700

    Added in some info on the profile page

--- UI/hooves_react/src/components/routes/Settings/Profile.js
+++ UI/hooves_react/src/components/routes/Settings/Profile.js
@@ -1,7 +1,18 @@
 function Profile() {
+    let tokenExpire = localStorage.getItem('TokenExpire');
+    let time = new Date(tokenExpire * 1000);
+    
     return (
         <div>
-            profile
+            <h2>
+                {localStorage.getItem('User')}
+            </h2>
+            <h3>
+                Auth Expire: {time.toLocaleString()} 
+            </h3>
+            <h3>
+                User Auth Token: {localStorage.getItem('Token')}
+            </h3>
         </div>
     )
 }
