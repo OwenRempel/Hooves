d0bb3267019131dc909d5d343456670e9dd9f81a
Owen <owenrempel@gmail.com>
2022-01-22 00:48:33 +0000

    Mobile menu updates

--- UI/hooves_react/src/components/Nav.jsx
+++ UI/hooves_react/src/components/Nav.jsx
@@ -1,27 +1,45 @@
+import { useState } from "react";
 import { Link } from "react-router-dom"
 
 function Nav() {
+    const [ToggleClass, setToggleClass] = useState('menuSmall');
+    const [Icon, setIcon] = useState('menu');
+    const menuToggle = () =>{
+       
+            if(ToggleClass === 'menuSmall'){
+                setToggleClass('menuBig');
+                setIcon('arrow_back')
+            }else{
+                setToggleClass('menuSmall')
+                setIcon('menu')
+            }
+      
+       
+    }
     return (
-        <nav className='menu'>
-            <Link to='/' >
-                <div>
+        <nav className={`menu ${ToggleClass}`}>
+            <span className='menuBackIcon' >
+                <i className="material-icons" id="menuic" onClick={menuToggle}>{Icon}</i>
+            </span>
+            <Link to='/' onClick={menuToggle}>
+                <div >
                     <span className="material-icons menuIcon">home</span>
                     <span>Home</span>
                 </div>
             </Link>
-            <Link to='/cows' >
+            <Link to='/cows' onClick={menuToggle}>
                 <div>
                     <span className="material-icons menuIcon">format_list_bulleted</span>
                     <span>List</span>
                 </div>
             </Link>
-            <Link to='/groups'>
+            <Link to='/groups' onClick={menuToggle}>
                 <div>
                     <span className="material-icons menuIcon">library_add</span>
                     <span>Groups</span>
                 </div>
             </Link>
-            <Link to='/feed' >
+            <Link to='/feed' onClick={menuToggle}>
                 <div>
                     <span className="material-icons menuIcon">grass</span>
                     <span>Feed</span>
