0c4edfac559e648b6c81accecb92c1df22ff23a1
Owen <owenrempel@gmail.com>
2022-01-22 00:48:15 +0000

    Updated to work with sub arrays

--- UI/hooves_react/src/components/DisplayItems.jsx
+++ UI/hooves_react/src/components/DisplayItems.jsx
@@ -1,17 +1,42 @@
-
+import Table from "./Table/Table";
+import { useParams } from "react-router-dom";
 function DisplayItems({ data }) {
+    const { ID } = useParams();
     let newArray = [];
-    const { Data, Info } = data
+    const { Data, Info, Sub } = data
     for(let item in Info){
         newArray.push({'name':Info[item], 'value':Data[0][item]})
     }
     
-    
     return (
         <div className="DisplayWrapper">
-            { newArray.map((item, i) => (
-                <div key={i} className="DisplayItem"><span>{item.name}</span><span>{item.value}</span></div>
-            ))}
+            <div className="cowInfo">
+                { newArray.map((item, i) => (
+                    <div key={i} className="DisplayItem"><span>{item.name}</span><span>{item.value}</span></div>
+                ))}
+            </div>
+            
+            <div className="subInfo">
+                {Object.keys(Sub).map((key, i) =>(
+                    <span key={i}>
+                        <h2>{key.charAt(0).toUpperCase() + key.slice(1)}</h2>
+                        <Table table={Sub[key]} UrlKey={
+                            [
+                                {
+                                    title:'Edit',
+                                    link:`/cows/${key}/edit/${ID}/`
+                                },
+                                {
+                                    title:'Delete',
+                                    link:`/cows/${key}/Delete/${ID}/`
+                                }
+                            ]
+                            }/> 
+                       
+                    </span>
+                ))}
+            </div>
+            
         </div>
     )
 }
