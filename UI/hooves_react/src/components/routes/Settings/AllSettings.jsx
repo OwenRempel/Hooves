182700fd0c9c8ed36e14521987fa5c630d0ff267
Owen <owenrempel@gmail.com>
2022-02-24 21:50:18 -0700

    Added link to the pens page

--- UI/hooves_react/src/components/routes/Settings/AllSettings.jsx
+++ UI/hooves_react/src/components/routes/Settings/AllSettings.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom"
+
 function AllSettings() {
     return (
         <div>
             <h1>All settings</h1>
+            <Link to='/settings/pens'>Pens</Link>
         </div>
     )
 }
