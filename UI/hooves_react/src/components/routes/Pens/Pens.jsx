6a8706dd430181fb2f5c57c8d36cbbc9543662ad
Owen Rempel <owenrempel@gmail.com>
2022-03-06 13:18:33 -0700

    Importing the needed parts instead of having it all in one page

--- UI/hooves_react/src/components/routes/Pens/Pens.jsx
+++ UI/hooves_react/src/components/routes/Pens/Pens.jsx
@@ -1,27 +1,25 @@
 import React from 'react'
 
-import { Outlet } from 'react-router-dom'
+import { useState } from 'react'
 
-function List(){
-    
-    return true
-}
-function Add(){
-    
-}
-function Delete(){
+import { Link } from 'react-router-dom'
 
-}
-function Edit(){
-
-}
+import PenList from './PenList'
+import PenAdd from './PenAdd'
+import PenDelete from './PenDelete'
+import PenEdit from './PenEdit'
 
 function Pens() {
+  const [Nav, setNav] = useState('list')
+  const [ID, setID] = useState(null)
     
   return (
       <>
-        <h3>Pens</h3>
-        <Outlet/>
+        <h2>Pens</h2>
+        <button onClick={() => {setNav('add')}}>Add</button>
+        {Nav === 'list' && <PenList/>}
+        {Nav === 'add' && <PenAdd/>}
+        {Nav === 'edit' && <PenEdit/>}
       </>
   )
 }
