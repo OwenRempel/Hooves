da941c697294f7ca7824a8d625e93fd5f63a0c86
Owen <owenrempel@gmail.com>
2022-03-10 16:53:33 -0700

    Adding list add and remove actions to the entries for groups

--- API/app/Extras/group.php
+++ API/app/Extras/group.php
@@ -43,19 +43,98 @@ if(isset($userData['Error'])){
 
 $DB = new DB($userData['DBName']);
 
+function getEntries($DB, $formData, $groupID ){
+    $groupData = $DB->query('SELECT data from '.$formData['tableName'].' WHERE ID=:id', array('id'=>$groupID));
+    echo json_encode($groupData);
+}
+
+
+function addEntry($DB, $formData, $groupID, $entryID){
+    $DataArray = [];
+    $groupData = $DB->query('SELECT data from '.$formData['tableName'].' WHERE ID=:id', array('id'=>$groupID));
+    $entryData = $DB->query('SELECT ID from '.$formData['entriesTarget'].' WHERE ID=:id', array('id'=>$entryID));
+    if(!isset($groupData[0])){
+        http_response_code(404);
+        echo stouts('That Group Dosen\'t Exist', 'error');
+        exit();
+    }else{
+        $groupData = $groupData[0];
+    }
+
+    if(!isset($entryData[0]['ID'])){
+        http_response_code(404);
+        echo stouts('That Entry Dosen\'t Exist', 'error');
+        exit();
+    }
+
+    if($groupData['data']){
+        $DataArray = json_decode($groupData['data'], 1);
+    }
+
+    if(in_array($entryID, $DataArray)){
+        echo stouts('Entry already in Group', 'success');
+        exit();
+    }
+    $DataArray[] = $entryID;
+    $sendData = json_encode($DataArray);
+    $DB->query('Update '.$formData['tableName'].' SET data=:group WHERE ID=:id', array('group'=>$sendData, 'id'=>$groupID));
+    $DB->query('Update '.$formData['entriesTarget'].' SET '.$formData['entriesLink'].'=:group WHERE ID=:id', array('group'=>$groupID, 'id'=>$entryID));
+    echo stouts('Entry added to Group', 'success');
+    exit();
+}
+
+function removeEntry($DB, $formData, $groupID, $entryID){
+    $DataArray = [];
+    $groupData = $DB->query('SELECT data from '.$formData['tableName'].' WHERE ID=:id', array('id'=>$groupID));
+    $entryData = $DB->query('SELECT ID from '.$formData['entriesTarget'].' WHERE ID=:id', array('id'=>$entryID));
+    if(!isset($groupData[0])){
+        http_response_code(404);
+        echo stouts('That Group Dosen\'t Exist', 'error');
+        exit();
+    }else{
+        $groupData = $groupData[0];
+    }
+
+    if(!isset($entryData[0]['ID'])){
+        http_response_code(404);
+        echo stouts('That Entry Dosen\'t Exist', 'error');
+        exit();
+    }
+
+    if($groupData['data']){
+        $DataArray = json_decode($groupData['data'], 1);
+    }
+
+    if(!in_array($entryID, $DataArray)){
+        echo stouts('Entry is not in Group', 'success');
+        exit();
+    }
+    
+    $DataArray = array_filter($DataArray, fn($value) => $value !== $entryID);
+
+    $sendData = json_encode($DataArray);
+    $DB->query('Update '.$formData['tableName'].' SET data=:group WHERE ID=:id', array('group'=>$sendData, 'id'=>$groupID));
+    $DB->query('Update '.$formData['entriesTarget'].' SET '.$formData['entriesLink'].'=null WHERE ID=:id', array('id'=>$entryID));
+    echo stouts('Entry removed from Group', 'success');
+    exit();
+}
+
+
 $groupFormData = [
     'loginAuth'=>true,
     'formDesc'=>'Group',
     'formName'=>'GroupMod',
     'tableName'=>'GroupData',
     'success'=>'Group successfully created!',
+    'entriesTarget'=>'Cattle',
+    'entriesLink'=>'GroupId',
     'items'=>[
         [
             'name'=>'GroupName',
             'typeName'=>'FormInput',
             'type'=>'text',
             'inputLabel'=>'Group Name',
-            
+            'unique'=>true
         ],
         [
             'name'=>'Pen',
@@ -70,15 +149,45 @@ $groupFormData = [
 
 switch ($method) {
     case 'GET':
-        if($Routes[1] == 'info'){
+        if($Routes[1] == 'info' and !isset($Routes[2])){
             http_response_code(200);
             getFormStruct($groupFormData, 'group');
+        }elseif($Routes[1] == 'info' and isset($Routes[2])){
+            selectUpdateFormItem($groupFormData, $Routes[0], $Routes[2]);
+        }elseif($Routes[1] == 'entries' and isset($Routes[2])){
+            getEntries($DB, $groupFormData, $Routes[2]);
         }else{
             selectFormData($groupFormData);
         }
         break;
     case 'POST':
-        insertFormData($PostData, $groupFormData, $Routes);
+        if($Routes[1] == 'entries'){
+            if(isset($Routes[4])){
+                switch ($Routes[3]) {
+                    case 'add':
+                        addEntry($DB, $groupFormData, $Routes[2], $Routes[4]);
+                        break;
+                    case 'remove':
+                        removeEntry($DB, $groupFormData, $Routes[2], $Routes[4]);
+                        break;
+                    default:
+                        http_response_code(404);
+                        echo stouts('Please Choose a valid action', 'error');
+                        break;
+                }
+            }else{
+                http_response_code(404);
+                echo stouts('This route is not valid', 'error');
+            }
+        }else{
+            insertFormData($PostData, $groupFormData, $Routes);
+        }
+        break;
+    case 'PUT':
+        updateFormData($PostData, $groupFormData, $Routes[1]);
+        break;
+    case 'DELETE':
+        deleteItem($groupFormData, $Routes[1]);
         break;
     default:
         http_response_code(405);
