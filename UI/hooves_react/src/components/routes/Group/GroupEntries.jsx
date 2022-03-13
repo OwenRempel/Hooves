8387a66a97ca1adae18353ac07a6679792fb4b9b
Owen <owenrempel@gmail.com>
2022-03-12 17:10:25 -0700

    Finalizing the Group Entries page

--- UI/hooves_react/src/components/routes/Group/GroupEntries.jsx
+++ UI/hooves_react/src/components/routes/Group/GroupEntries.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom"
 
 
 function GroupEntries() {
-    const [ Entries, setEntries] = useState();
+    const [ Entries, setEntries] = useState({});
     const { ID } = useParams();
     useEffect(() => {
     fetch(process.env.REACT_APP_API_URL+'/group/'+ID+'/entries?token='+localStorage.getItem('Token'))
@@ -12,12 +12,69 @@ function GroupEntries() {
             setEntries(result)
             })
     }, [ID]);
-    
-    function List(data, type){
-        
+
+    const Thead = ({info}) =>{
+      return(
+        <thead>
+          <tr>
+            <th></th>
+            {Object.keys(info).map((key, i)=>{
+              if(key !== "ID"){
+                return <th key={i}>{info[key]}</th>
+              }else{
+                return null
+              }
+            })}
+          </tr>
+        </thead>
+      )
     }
-    
-    
+    const Trow = ({row, type}) =>{
+      return(
+        <tr>
+          {type === 'remove' &&
+            <td className="linkClick" onClick={()=>removeEntry(row.ID)}>Remove</td>
+          }
+          {type === 'add' &&
+            <td className="linkClick" onClick={()=>addEntry(row.ID)}>Add</td>
+          }
+            {Object.keys(row).map((key, i)=>{
+              if(key !== "ID"){
+                return <th key={i}>{row[key]}</th>
+              }else{
+                return null
+              }
+            })}
+        </tr>
+      )
+    }
+    const Tbody = ({data, type}) =>{
+      return(
+        <tbody>
+          {data.map((item, i)=>(
+            <Trow key={i} type={type} row={item}/>
+          ))}
+
+        </tbody>
+      )
+    }
+    const List = ({data, type}) =>{
+      return(
+        <>
+          <table>
+            <Thead info={data.Info}/>
+            <Tbody type={type} data={data.Data}/>
+          </table>
+        </>
+      )
+    }
+    const searchEntrys = (e) =>{
+      var val = e.target.value
+      
+    }
+    const removeEntry = (EntryID) =>{
+        console.log(EntryID);
+    }
 
     const addEntry = (EntryID) =>{
         console.log(EntryID);
@@ -25,6 +82,13 @@ function GroupEntries() {
   return (
     <>
         <h2>Group Items</h2>
+        {Entries.Data &&
+          <List data={Entries} type='remove'/>
+        }
+        <div className="groupSearch">
+          <input type='search' onKeyUp={searchEntrys}/>
+        </div>
+        
     </>
   )
 }
