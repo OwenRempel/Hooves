c2618f4499b0777ed4813d74c91d5c9f0aaa48eb
Owen <owenrempel@gmail.com>
2022-03-15 17:05:05 -0700

    Changing some styles

--- UI/hooves_react/src/components/Search.jsx
+++ UI/hooves_react/src/components/Search.jsx
@@ -2,7 +2,7 @@
 function Search() {
     return (
         <div className='search'>
-            <input type="search" className='searchInput' placeholder='Search'/>
+            <input type="search" className='searchInput mobHide' placeholder='Search'/>
         </div>
     )
 }
