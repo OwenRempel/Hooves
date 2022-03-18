9eacc5c7c9799376db4165435b0188be080ef11d
Owen <owenrempel@gmail.com>
2022-03-17 22:30:06 -0700

    Working on the location groups on the UI side

--- UI/hooves_react/src/components/Table/Table.jsx
+++ UI/hooves_react/src/components/Table/Table.jsx
@@ -2,13 +2,38 @@ import TableBody from "./TableBody"
 import TableHead from "./TableHead"
 import '../../css/Table.css'
 
-function Table({ table, UrlKey }) {
+function Table({ stick, table, UrlKey }) {
+    const TablePart = (parms) =>{
+       const { body, info } = parms
+        return(
+            <table className={`BuildTable highlight ${stick && 'stick'}`}>
+                <TableHead Head={info}/>
+                <TableBody Body={body} UrlKey={UrlKey} />
+            </table>
+        )
+    }
+    const Locations = (props) => {
+        const { data } = props
+        const loc = data.Data.Locations
+        return(
+           <>
+           {Object.keys(loc).map((i, key)=>(
+                <div key={'mit'+key}>
+                    <h2 key={'itm'+key}>{i}</h2>
+                    <TablePart key={key} info={data.Info} body={loc[i]}/>
+                </div>
+           ))
+           }
+           </> 
+        )
+    }
     return (
         <div className="tableCatch">
-            <table className="BuildTable highlight">
-                <TableHead Head={table.Info}/>
-                <TableBody Body={table.Data} UrlKey={UrlKey} />
-            </table>
+            {!table.Data.Locations ? 
+                <TablePart info={table.Info} body={table.Data}/>
+                :
+                <Locations data={table}/>
+            }
         </div>
         
     )
