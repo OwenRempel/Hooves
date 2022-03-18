7a21cc36371d4eadb9b45e0330437a93386cf9b0
Owen <owenrempel@gmail.com>
2022-03-18 15:47:39 -0700

    Style change to the back button so that it has an arrow

--- UI/hooves_react/src/components/Back.jsx
+++ UI/hooves_react/src/components/Back.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 
 function Back({ link }) {
-    return <Link to={link}><button className="btn">Back</button></Link>
+    return <Link to={link}><button className="btn btn-small"><i className="arrow left"></i> Back</button></Link>
 }
 
 export default Back
