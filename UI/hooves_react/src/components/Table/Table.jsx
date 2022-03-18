602ef5498b8635c2f0451cda7ceb918923652730
Owen <owenrempel@gmail.com>
2022-03-18 15:50:38 -0700

    Added state so that only the wanted pen is displayed

--- UI/hooves_react/src/components/Table/Table.jsx
+++ UI/hooves_react/src/components/Table/Table.jsx
@@ -1,41 +1,79 @@
 import TableBody from "./TableBody"
 import TableHead from "./TableHead"
 import '../../css/Table.css'
+import { useEffect, useState } from "react"
 
 function Table({ stick, table, UrlKey }) {
+    const [Tables, setTables] = useState(null)
+        useEffect(() => {
+            if(table.Data.Locations){
+                const locate = table.Data.Locations
+                Object.keys(locate).map((item, i)=>{
+                    if(i === 0){
+                        setTables(item)
+                    }
+                    return false               
+                })
+            }
+        }, [table])
+
     const TablePart = (parms) =>{
        const { body, info } = parms
         return(
-            <table className={`BuildTable highlight ${stick && 'stick'}`}>
-                <TableHead Head={info}/>
-                <TableBody Body={body} UrlKey={UrlKey} />
-            </table>
+            <div className="tableCatch">
+                <table className={`BuildTable highlight ${stick && 'stick'}`}>
+                    <TableHead Head={info}/>
+                    <TableBody Body={body} UrlKey={UrlKey} />
+                </table>
+            </div>
+            
         )
     }
     const Locations = (props) => {
         const { data } = props
         const loc = data.Data.Locations
+        let title = Tables
         return(
            <>
-           {Object.keys(loc).map((i, key)=>(
-                <div key={'mit'+key}>
-                    <h2 key={'itm'+key}>{i}</h2>
-                    <TablePart key={key} info={data.Info} body={loc[i]}/>
-                </div>
-           ))
-           }
+                {title &&
+                    <div>
+                        <h2>{title}</h2>
+                        <TablePart info={data.Info} body={loc[title]}/>
+                    </div>    
+                }
            </> 
         )
     }
+    const SelectTable = (props) =>{
+
+        if(!Tables) return false
+        const { items } = props
+        
+        const catchSelect = (e) =>{
+            let val = e.target.textContent
+            setTables(val)
+        }
+        return(
+            <>
+            <br></br>
+                {Object.keys(items).map((i, key)=>(
+                <button className={`btn btn-small ${Tables === i ? 'yes-btn' : ''}`} key={key} onClick={catchSelect}>{i}</button>
+                ))}
+            </>
+        )
+    }
+    
     return (
-        <div className="tableCatch">
+        <>
             {!table.Data.Locations ? 
                 <TablePart info={table.Info} body={table.Data}/>
                 :
-                <Locations data={table}/>
+                <>
+                    <SelectTable items={table.Data.Locations}/>
+                    <Locations data={table}/>
+                </>
             }
-        </div>
-        
+        </>
     )
 }
 
