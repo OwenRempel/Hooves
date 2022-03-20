7085b3ce1f417def4d4c0e66008cd6fbc8c60c1a
Owen Rempel <owenrempel@gmail.com>
2022-03-20 16:51:20 -0600

    moving company add to its own file

---
+++ API/app/Views/company-add.php
@@ -0,0 +1,11 @@
+<?php
+
+$DB = new DB_Admin;
+
+$uid = bin2hex(random_bytes(24));
+
+$DB->query('INSERT into CompCreateAuth (Token, Expire) Values (:token, :expire)', array(
+    'token'=> $uid,
+    'expire'=>date('Y-m-d H:i:s', (time()+(60 * 24 * 10)))
+));
+echo "http://".$_SERVER['SERVER_NAME'].":3000/companies/add?token=$uid";
