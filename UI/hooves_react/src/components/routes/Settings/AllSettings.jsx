0b0dc54bfe542d49945c490d5ba7ec225117d537
Owen <owenrempel@gmail.com>
2022-03-21 16:29:23 -0700

    Adding a new link to settings

--- UI/hooves_react/src/components/routes/Settings/AllSettings.jsx
+++ UI/hooves_react/src/components/routes/Settings/AllSettings.jsx
@@ -5,6 +5,7 @@ function AllSettings() {
         <>
             <h1>All settings</h1>
             <Link to='/settings/pens' className="btn">Pens</Link>
+            <Link to='/settings/view-items' className="btn">Display Items</Link>
         </>
     )
 }
