c07f8d5a20e4f7261ee6d49ba31715c5d8ff9da7
Owen <owenrempel@gmail.com>
2022-03-21 16:28:54 -0700

    Adding a route for updating settings

---
+++ API/app/Extras/settings.php
@@ -0,0 +1,109 @@
+<?php
+
+// Resources $localarray, $PostData, $Routes, $FormBuilderArray
+
+$PHPinput = file_get_contents('php://input');
+$PostInput = json_decode($PHPinput, 1);
+parse_str($PHPinput, $_PUT);
+//echo json_encode($PostInput);
+//This is the check to see if the api should use $_POST or php://input
+if($method == 'POST' or $method == 'PUT'){
+    if(isset($_POST['SettingsMod'])){
+        $PostData = $_POST;
+    }elseif(isset($_PUT['SettingsMod'])){
+        $PostData = $_PUT;
+    }elseif(isset($PostInput['SettingsMod'])){
+        $PostData = $PostInput;
+    }else{
+        http_response_code(400);
+        echo stouts('No data Received', 'error');
+        exit();
+    }
+}
+
+
+
+if(!isset($_GET['token']) and !isset($PostData['Token'])){
+    http_response_code(401);
+    echo stouts('Please include token parm', 'error');
+    exit();
+}
+if(isset($_GET['token'])){
+    $userData = getInfoFromToken($_GET['token']);
+}else{
+    $userData = getInfoFromToken($PostData['Token']);
+}
+
+
+if(isset($userData['Error'])){
+    echo stouts($userData['Error'], 'error');
+    exit();
+}
+
+
+$DB = new DB($userData['DBName']);
+$DB_Admin = new DB_Admin;
+
+$Builditems = $FormBuilderArray['Routes']['cattle']['items'];
+
+function addLabels($arr){
+    Global $FormBuilderArray;
+    $items = $FormBuilderArray['Routes']['cattle']['items'];
+    $out = [];
+    foreach($items as $item){
+        
+        if(isset($arr[$item['name']])){
+            $out[$item['name']] = [
+                'value'=>$arr[$item['name']],
+                'name'=>(isset($item['inputLabel']) ? $item['inputLabel'] : $item['name'])
+            ];
+        }
+    }
+   
+    return $out;
+}
+
+
+if($Routes[1] == 'view-items'){
+    $da = $DB_Admin->query('SELECT ListDisplayPref FROM Companies WHERE DBName=:db', array('db'=>$userData['DBName']));
+   
+    if($method == 'GET'){
+        if($Routes[2] == 'info'){
+            if($da[0]['ListDisplayPref'] == null){ 
+                $arrayOut = [];
+                //TODO: This will have to be updated to allow for weights and other items
+                foreach($Builditems as $item){
+                    $name = $item['name'];
+                    $arrayOut[$name] = true;
+                }
+
+                echo json_encode(addLabels($arrayOut));
+            }else{
+                echo json_encode(addLabels(json_decode($da[0]['ListDisplayPref'], 1)));
+            }
+        }else{
+            http_response_code(404);
+            echo stouts('That view dosen\'t Exist', 'error');
+            exit();
+        }
+    }elseif($method == 'POST'){
+        $build = [];
+        foreach($Builditems as $item){
+            $name = $item['name'];
+            if(isset($PostData[$name])){
+                $build[$name] = $PostData[$name];
+            }
+        }
+       
+        $DB_Admin->query('UPDATE Companies SET ListDisplayPref=:di WHERE DBName=:db', array('di'=>json_encode($build),'db'=>$userData['DBName']));    
+        http_response_code(200);
+        echo stouts('View Items Updated', 'success');
+        exit();
+    }
+    
+
+}else{
+    http_response_code(404);
+    echo stouts('That view dosen\'t Exist', 'error');
+    exit();
+}
