1b45918792ed561a5c3ad5841960f113cb7eb2eb
Owen Rempel <owenrempel@gmail.com>
2022-04-03 23:24:58 -0600

    Had to run this so added some new data to the gen file

--- API/app/Views/gen.php
+++ API/app/Views/gen.php
@@ -1,14 +1,14 @@
 <?php
-/*
+  /*
 
 
-$DB = new DB('Hoves$2$TheGreatCompany$7ac50e39d8a2cd16');
+$DB = new DB('Hoves$2$RempelCattleCo$eb1eece2d1c37dbe');
 $i = 0;
 while($i < 100){
     $users = ['Adam', 'Caleb', 'John', 'Dusty', 'Rose', 'Jen', 'Tyler'];
     $age = ['Heffer', 'Steer', 'Bull'];
     $des = ['This is a cow', 'What a great cow', 'This cow is a bad one', 'Look out for this one'];
-    $pens = [1, 2, 3, 4, ''];
+    $pens = [1, 2, 3, ''];
     do{
         $secUUID = bin2hex(random_bytes(24));
     }while(!empty($DB->query("SELECT ID from Cattle WHERE ID='$secUUID'")));
@@ -29,7 +29,6 @@ while($i < 100){
     $pen = $pens[array_rand($pens)];
     $desc = $des[array_rand($des)];
     echo "$Tag -- $ID\n";
-    $DB->query("INSERT INTO Cattle (Tag, BuyDate, HerdsMan, Investor, Price, AgeState, PenDate, Pen, Description, ID) VALUES ('$Tag', '$buyDate', '$invest', '$owner', '$price', '$type', '$penDate', '$pen', '$desc', '$ID')");
+    $DB->query("INSERT INTO Cattle (Tag, StartDate, HerdsMan, Investor, Price, AgeState, PenDate, Pen, Description, ID) VALUES ('$Tag', '$buyDate', '$invest', '$owner', '$price', '$type', '$penDate', '$pen', '$desc', '$ID')");
     $i++;
 }
-*/
