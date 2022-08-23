c946e909d69ebf8b14a5255a64e41fa59e68ed77
Owen <owenrempel@gmail.com>
2022-08-23 14:54:56 -0700

    Added state to manage the rows that have been selected Started on some buttons to run actions on the entire selection

--- UI/hooves_react/src/components/routes/Cows/CowsList.jsx
+++ UI/hooves_react/src/components/routes/Cows/CowsList.jsx
@@ -5,6 +5,7 @@ import Table from '../../Table/Table';
 
 function CowsList() {
     const [AllCows, setAllCows] = useState({});
+    const [GroupOptions, setGroupOptions] = useState([])
     let feedlot = 0;
     if(localStorage.getItem('Feedlot')){
        feedlot = parseInt(localStorage.getItem('Feedlot'));
@@ -24,6 +25,19 @@ function CowsList() {
               setAllCows(result)
             })
     }, [feedlot]);
+   
+    const handleGroupSelect = (e) => {
+      if(e.target.checked){
+        setGroupOptions([...GroupOptions, e.target.value])
+      }else{
+         setGroupOptions(GroupOptions.filter(id =>id !== e.target.value))
+      }
+    }
+
+    const groupDelete = () => {
+      console.log('Selected Cows Have Been deleted');
+      setGroupOptions([]);
+    }
     return (
         <>
             <Back link='/'/>
@@ -32,7 +46,15 @@ function CowsList() {
               {!AllCows.Data.Locations &&
                 <h1>All Cattle</h1>
               }
-              <Table stick={true} table={AllCows} UrlKey={[{title:'View',link:'/cows/', className:'btn btn-small'}]}/>
+              {GroupOptions.length !== 0 &&
+                <div className='groupOptionsDiv'>
+                  <h3>Actions for selected items</h3>
+                  <button className='btn no-btn btn-small' onClick={groupDelete}>Delete</button>
+                  <button className='btn btn-small'>Move</button>
+                  <button className='btn btn-small'>Add to Group</button>
+                </div>
+              }
+              <Table stick={true} table={AllCows} UrlKey={[{title:'View',link:'/cows/', className:'btn btn-small'}]} groupSelect={true} groupList={GroupOptions} groupOnChange={handleGroupSelect}/>
             </>
             }
             
